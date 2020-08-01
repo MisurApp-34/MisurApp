280ff69ab1359d02c0438ced17116d4a0b6a8676
ariannapesce <annairafishy@gmail.com>
2020-08-01 13:48:57 +0200

    Modifica di strings.xml, colors.xml Aggiunta file single_tool.xml per la creazione del layout relativo allo specifico strumento Aggiunta dell'immagine in background bg.png Aggiunta dei file historical.xml e pre_bottom.xml per la creazione dello stile dei bottoni (colore e forma) della corrispettiva schermata

--- app/src/main/java/it/uniba/di/misurapp/MainActivity.java
+++ app/src/main/java/it/uniba/di/misurapp/MainActivity.java
@@ -1,11 +1,5 @@
 package it.uniba.di.misurapp;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.cardview.widget.CardView;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,6 +7,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.SearchView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
