cb6d3f2a51c2e6c62bca28c3da530e9f0f0b5c14
fabio <fabio.palladino96@hotmail.com>
2020-08-04 13:33:59 +0200

    Modifica di ProximityTool.java con aggiunta di grafico e valore  e aggiunta di stringhe in strings.xml

--- app/src/main/java/it/uniba/di/misurapp/ProximityTool.java
+++ app/src/main/java/it/uniba/di/misurapp/ProximityTool.java
@@ -2,22 +2,49 @@ package it.uniba.di.misurapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
 import android.widget.TextView;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
 public class ProximityTool extends AppCompatActivity{
     TextView ProximitySensor, data;
     SensorManager mySensorManager;
     Sensor myProximitySensor;
+    private LineChart mChart;
+    private Thread thread;
+    private boolean plotData = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.single_tool);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        try {
+            getSupportActionBar().setTitle(R.string.proximity_details);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
         ProximitySensor = (TextView) findViewById(R.id.details);
         data = (TextView) findViewById(R.id.measure);
         mySensorManager = (SensorManager) getSystemService(
@@ -25,14 +52,85 @@ public class ProximityTool extends AppCompatActivity{
         myProximitySensor = mySensorManager.getDefaultSensor(
                 Sensor.TYPE_PROXIMITY);
         if (myProximitySensor == null) {
-            ProximitySensor.setText("Nessun sensore di profondità");
+            ProximitySensor.setText(R.string.nosensor);
         } else {
+
+            ProximitySensor.setText(R.string.proximity_details);
             mySensorManager.registerListener(proximitySensorEventListener,
                     myProximitySensor,
                     SensorManager.SENSOR_DELAY_NORMAL);
         }
+
+        //associo il chart del layout alla variabile LineChart
+        mChart = (LineChart) findViewById(R.id.chart1);
+
+        // disabilito label descrizione chart
+        mChart.getDescription().setEnabled(false);
+
+        // abilito interazione touch con il grafico
+        mChart.setTouchEnabled(true);
+
+        // abilito scaling
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+
+        //abilito pinch to zoom
+        mChart.setPinchZoom(true);
+
+        // definisco background chart
+        mChart.setBackgroundColor(Color.WHITE);
+
+        //Creo oggetti data e definisco colore del testo dei dati
+        LineData data = new LineData();
+        data.setValueTextColor(Color.BLACK);
+
+        // aggiungo inzialmente dati vuoti
+        mChart.setData(data);
+
+        // richiamo leggenda
+        Legend l = mChart.getLegend();
+
+        // modifico leggenda
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.BLACK);
+
+        XAxis xl = mChart.getXAxis();
+        xl.setTextColor(Color.WHITE);
+        xl.setDrawGridLines(true);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setAxisMaximum(10f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setEnabled(true);
+
+        mChart.getAxisLeft().setDrawGridLines(true);
+        mChart.getXAxis().setDrawGridLines(false);
+        mChart.setDrawBorders(false);
+
+        feedMultiple();
+
+        // recupero il valore selezionato nel grafico, per poter eventualmente salvarlo.
+        mChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+                String value = mChart.getXAxis().getValueFormatter().getFormattedValue(e.getY(), mChart.getXAxis());
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
     }
-    
+
 
     SensorEventListener proximitySensorEventListener
             = new SensorEventListener() {
@@ -45,13 +143,105 @@ public class ProximityTool extends AppCompatActivity{
             // TODO Auto-generated method stub
             if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
                 if (event.values[0] == 0) {
-                    data.setText("Near");
+
+                    data.setText((Double.toString(event.values[0]))+"cm");
+                   // data.setText(R.string.near);
                 } else {
-                    data.setText("Away");
+                   // data.setText(R.string.far);
+                    data.setText((Double.toString(event.values[0]))+"cm");
+
+                }
+
+                //invio evento ed attributi al metodo addEntry che aggiungerà gli elementi al grafico
+                if (plotData) {
+                    addEntry(event);
+                    plotData = false;
                 }
             }
         }
     };
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (thread != null) {
+            thread.interrupt();
+        }
+        mySensorManager.unregisterListener((SensorEventListener) this);
 
+    }
+
+    private void feedMultiple() {
+
+        if (thread != null) {
+            thread.interrupt();
+        }
+
+        thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true) {
+                    plotData = true;
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+    private void addEntry(SensorEvent event) {
+
+        LineData data = mChart.getData();
+
+        if (data != null) {
+
+            ILineDataSet set = data.getDataSetByIndex(0);
+
+            if (set == null) {
+                set = createSet();
+                data.addDataSet(set);
+            }
+
+
+            data.addEntry(new Entry(set.getEntryCount(), event.values[0]), 0);
+            data.notifyDataChanged();
+
+            // mostra il cambiamento dei dati presenti nel chart
+            mChart.notifyDataSetChanged();
+
+//numero di elementi visibili nel chart prima dello scroll automatico
+            mChart.setVisibleXRangeMaximum(15);
+
+            // muovi l'ultimo elemento
+            mChart.moveViewToX(data.getEntryCount());
+
+        }
+    }
+
+    //creo il tracciato nel grafico
+    private LineDataSet createSet() {
+
+
+        LineDataSet set = new LineDataSet(null, "");
+        set.setAxisDependency(YAxis.AxisDependency.RIGHT);
+        set.setLineWidth(1f);
+        set.setColor(Color.BLUE);
+        set.setHighlightEnabled(true);
+        set.setDrawValues(true);
+        set.setDrawCircles(true);
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setCubicIntensity(0.2f);
+        return set;
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 
 }
