a3a83ff29ac62320de42d97b8592be61e4c6dd51
e.valente5 <e.valente5@studenti.uniba.it>
2020-09-01 18:02:45 +0200

    Aggiunta Toolbar in Settings.java aggiungendo un PreferanceFragment Aggiunta stringa titolo toolbar sezione Settings Personalizzato stile PreferenceCategory  in xml/settings.xml creando layout in preferences_category Creato layout/settings.xml Activity Settings.java

--- app/src/main/java/it/uniba/di/misurapp/Settings.java
+++ app/src/main/java/it/uniba/di/misurapp/Settings.java
@@ -1,5 +1,8 @@
+
 package it.uniba.di.misurapp;
 
+import android.annotation.SuppressLint;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,129 +12,159 @@ import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import java.util.Locale;
 
-public class Settings extends PreferenceActivity {
+public class Settings extends  AppCompatActivity {
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+        //importo toolbar
 
-    Locale myLocale;
-    public String pressure_unit;
-    public String temperature_unit;
-    public  String speed_unit;
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        try {
+            getSupportActionBar().setTitle(R.string.settings);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        FragmentTransaction transaction =
+                getFragmentManager().beginTransaction();
 
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).replace(R.id.frameLayout, new MyPreferenceFragment())
+                .commitAllowingStateLoss();
+
+
+
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-
-        super.onCreate(savedInstanceState);
-
-        addPreferencesFromResource(R.xml.settings);
-
-        Preference langPreference = getPreferenceScreen().findPreference(
-            "languages");
-        langPreference.setOnPreferenceChangeListener(languageChangeListener);
-
-
-        Preference tempPreference = getPreferenceScreen().findPreference(
-                "temperature");
-        tempPreference.setOnPreferenceChangeListener(temperatureChangeListener);
-
-        Preference pressPreference = getPreferenceScreen().findPreference(
-                "pressure");
-        pressPreference.setOnPreferenceChangeListener(pressureChangeListener);
-
-        Preference speedPreference = getPreferenceScreen().findPreference(
-                "speed");
-       speedPreference.setOnPreferenceChangeListener(speedChangeListener);
-
-}
-
-    Preference.OnPreferenceChangeListener languageChangeListener = new Preference.OnPreferenceChangeListener() {
-
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object newValue) {
-
-            switch (newValue.toString()) {
-                case "en":
-                    setLocale("en");
-                    Toast.makeText(getApplicationContext(), "English!", Toast.LENGTH_LONG).show();
-                    break;
-
-                case "it":
-                    setLocale("it");
-                    Toast.makeText(getApplicationContext(), "Italian", Toast.LENGTH_LONG).show();
-                    break;
-            }
-            return false;
-        }
-    };
-
-    Preference.OnPreferenceChangeListener temperatureChangeListener = new Preference.OnPreferenceChangeListener() {
-
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object newValue) {
-
-            SharedPreferences settings = getSharedPreferences("settings", 0);
-            SharedPreferences.Editor editor = settings.edit();
-            editor.putString("temperature",newValue.toString());
-            editor.commit();
-
-
-            return false;
-        }
-    };
-
-    Preference.OnPreferenceChangeListener pressureChangeListener = new Preference.OnPreferenceChangeListener() {
-
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object newValue) {
-
-            SharedPreferences settings = getSharedPreferences("settings", 0);
-            SharedPreferences.Editor editor = settings.edit();
-            editor.putString("pressure",newValue.toString());
-            editor.commit();
-
-
-            return false;
-        }
-    };
-
-
-
-    Preference.OnPreferenceChangeListener speedChangeListener = new Preference.OnPreferenceChangeListener() {
-
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object newValue) {
-
-            SharedPreferences settings = getSharedPreferences("settings", 0);
-            SharedPreferences.Editor editor = settings.edit();
-            editor.putString("speed",newValue.toString());
-            editor.commit();
-
-
-
-            return false;
-        }
-    };
-
-
-    //* manually changing current locale/
-    public void setLocale(String lang) {
-        myLocale = new Locale(lang);
-        Resources res = getResources();
-        DisplayMetrics dm = res.getDisplayMetrics();
-        Configuration conf = res.getConfiguration();
-        conf.locale = myLocale;
-        res.updateConfiguration(conf, dm);
-        Intent refresh = new Intent(this, Settings.class);
-        finish();
-        startActivity(refresh);
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
+
+
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings);
+            Preference langPreference = getPreferenceScreen().findPreference(
+                    "languages");
+            langPreference.setOnPreferenceChangeListener(languageChangeListener);
+
+
+            Preference tempPreference = getPreferenceScreen().findPreference(
+                    "temperature");
+            tempPreference.setOnPreferenceChangeListener(temperatureChangeListener);
+
+            Preference pressPreference = getPreferenceScreen().findPreference(
+                    "pressure");
+            pressPreference.setOnPreferenceChangeListener(pressureChangeListener);
+
+            Preference speedPreference = getPreferenceScreen().findPreference(
+                    "speed");
+            speedPreference.setOnPreferenceChangeListener(speedChangeListener);
+        }
+        Preference.OnPreferenceChangeListener languageChangeListener = new Preference.OnPreferenceChangeListener() {
+
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+                switch (newValue.toString()) {
+                    case "en":
+                        setLocale("en");
+                        break;
+
+                    case "it":
+                        setLocale("it");
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + newValue.toString());
+                }
+                return true;
+            }
+        };
+        //* manually changing current locale/
+        public void setLocale(String lang) {
+            Locale myLocale = new Locale(lang);
+            Resources res = getResources();
+            DisplayMetrics dm = res.getDisplayMetrics();
+            Configuration conf = res.getConfiguration();
+            conf.locale = myLocale;
+            res.updateConfiguration(conf, dm);
+            Intent refresh = new Intent(getActivity(),Settings.class);
+            getActivity().finish();
+            startActivity(refresh);
+
+        }
+        Preference.OnPreferenceChangeListener temperatureChangeListener = new Preference.OnPreferenceChangeListener() {
+
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+                SharedPreferences settings = getActivity().getSharedPreferences("settings",0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString("temperature",newValue.toString());
+                editor.commit();
+
+
+                return true;
+            }
+        };
+
+        Preference.OnPreferenceChangeListener pressureChangeListener = new Preference.OnPreferenceChangeListener() {
+
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+                SharedPreferences settings = getActivity().getSharedPreferences("settings",0);
+
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString("pressure",newValue.toString());
+                editor.commit();
+
+
+                return true;
+            }
+        };
+
+
+
+        Preference.OnPreferenceChangeListener speedChangeListener = new Preference.OnPreferenceChangeListener() {
+
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+                SharedPreferences settings = getActivity().getSharedPreferences("settings", 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString("speed",newValue.toString());
+                editor.commit();
+
+
+
+                return true;
+            }
+        };
+    }
+
 
 }
